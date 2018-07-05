5217a12abb683dae7515bb547b790c97048cf9d9
MEDIA\jquentin <jeremy.quentin@gmail.com>
2018-07-05 11:33:23 +0800

    Test2 - commit2

--- SpriteAnimation.cs
+++ SpriteAnimation.cs
@@ -2,4 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (SpriteRenderer))]
+public class SpriteAnimation : MonoBehaviour 
+{
 
+    public List<Sprite> sprites;
+
+    public int frameRate = 10;
+
+    float timeBetweenFrames
+    {
+        get
+        {
+            return 1f / frameRate;
+        }
+    }
+
+    int currentSpriteIndex;
+
+    float timeNextFrame;
+
+    SpriteRenderer _spriteRenderer;
+    SpriteRenderer spriteRenderer
+    {
+        get
+        {
+            if (_spriteRenderer == null)
+                _spriteRenderer = GetComponent <SpriteRenderer> ();
+            return _spriteRenderer;
+        }
+    }
+
+    void Start ()
+    {
+        currentSpriteIndex = 0;
+        timeNextFrame = Time.time + timeBetweenFrames;
+        UpdateSprite ();
+    }
+
+    void Update ()
+    {
+        if (Time.time >= timeNextFrame)
+        {
+            currentSpriteIndex = (currentSpriteIndex + 1) % sprites.Count;
+            timeNextFrame = timeNextFrame + timeBetweenFrames;
+            UpdateSprite ();
+        }
+    }
+
+    void UpdateSprite ()
+    {
+        spriteRenderer.sprite = sprites [currentSpriteIndex];
+    }
+	
+}
